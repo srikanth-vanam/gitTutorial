9d9bf9ade60b9daecb188794f3379cf1647640b3
srikanth-vanam <B172320@rgukt.ac.in>
2023-07-19 22:38:20 +0530

    13th question

--- index.js
+++ index.js
@@ -22,18 +22,35 @@ function onSubmit(e) {
   };
 
   // Store the object in local storage with the email as the key
-  localStorage.setItem("key",email);
   localStorage.setItem(email, JSON.stringify(formData));
 
   // Create a new paragraph element and set its content to the form data
-  const paragraph = document.createElement('p');
-  paragraph.textContent = `${username} - ${email} - ${phoneNumber}`;
+  const list = document.createElement('li');
+  list.textContent = `${username} - ${email} - ${phoneNumber}`;
 
   // Append the paragraph element to the "details" div
-  detailsDiv.appendChild(paragraph);
+  detailsDiv.appendChild(list);
+
+  // 13th question --> adding delete button to li content
+  const deleteBtn = document.createElement('button');
+  deleteBtn.textContent='Delete';
+  deleteBtn.id='deleter';
+  list.appendChild(deleteBtn);
 
   // Clear input fields after submission
   document.getElementById('username').value = '';
   document.getElementById('email').value = '';
   document.getElementById('number').value = '';
+}
+
+var del=document.getElementById('details');
+del.addEventListener('click',deletItem);
+function deletItem(e){
+    let parent=e.target.parentElement;
+    let array=e.target.parentElement.textContent.split('-');
+    // console.log(array);
+    let key=array[1].trim();
+    localStorage.removeItem(key);
+    del.removeChild(parent);
+    // console.log(array[1].trim());
 }
