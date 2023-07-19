92b1cf344c911a4bb855ccee474d86b95e490f56
srikanth-vanam <B172320@rgukt.ac.in>
2023-07-19 14:50:31 +0530

    9th question solution

--- modifier.js
+++ modifier.js
@@ -6,7 +6,7 @@ function removeBtn(e){
     if(e.target.classList.contains("delete")){
         // console.log(1);
         if(confirm("Are u sure ?")){
-            var li=e.target.parentElement;
+            let li=e.target.parentElement;
             listItems.removeChild(li);
         }
     }
@@ -25,7 +25,11 @@ function addListItem(e){
     newLi.className='list-group-item';
     newLi.appendChild(document.createTextNode(inputValue));
     console.log(inputValue);
-    // adding btn
+    // 9th question part -> adding only discription to li tag
+    let discriptionValue=document.getElementById('discription').value;
+    newLi.appendChild(document.createTextNode(" "+discriptionValue));
+    console.log(discriptionValue);
+    // adding delete and eidt btns
     let btn=document.createElement('button');
     btn.className="btn btn-danger btn-sm float-right delete";
     btn.textContent="X";
@@ -38,4 +42,32 @@ function addListItem(e){
     newLi.appendChild(editBtn);
     // adding li to ul
     listItems.appendChild(newLi);
+}
+
+//search filter 9th quesiton 
+let filter=document.getElementById('filter');
+filter.addEventListener('keyup',itemsFilter);
+
+function itemsFilter(e){
+    var str=e.target.value.toLowerCase();
+    var li=document.getElementsByTagName('li');
+    // console.log(str);
+    Array.from(li).forEach(item=>{
+        // checking first text and second text
+        // This line also works--->item.childNodes[1].textContent.toLowerCase().indexOf(str)!=-1)
+        if(item.firstChild.textContent.toLowerCase().indexOf(str)!=-1 || item.firstChild.nextSibling.textContent.toLowerCase().indexOf(str)!=-1){
+            let str1=document.getElementById('discription').value.toLowerCase();
+            let str2=item.firstChild.nextSibling.textContent.trim().toLowerCase();
+            if( str1===str2){
+                console.log(1);
+                let diiv=document.createElement('p');
+                diiv.appendChild(document.createTextNode(str1));
+                item.appendChild(diiv);
+            }
+            item.style.display='block'
+        }
+        else{
+            item.style.display='none'
+        }
+    })
 }
